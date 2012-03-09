bb0695258eb9dc67862f160b141a45b241a7c8e8
Asbjørn Clemmensen <ac@siteimprove.com>
2012-03-09 13:38:26 +0100

    Link-hack til Horsens, så alle links automatisk bærer den nødvendige parameter videre.

--- feedback.js
+++ feedback.js
@@ -474,6 +474,12 @@ szfbjQuery.noConflict();
 
 			state.set('init');
 
+			// Link-hack mens i udvikling.
+			$('a').each(function() {
+				var href = $(this).attr('href');
+				$(this).attr('href', href + ((href.indexOf('?') > -1) ? '&' : '?') + 'actest=' + Date.now());
+			});
+
 			_szfb = {
 				reload: function(new_opts) {
 					opts = $.extend(opts, new_opts);
